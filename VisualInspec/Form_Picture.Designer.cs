b9fc172f8babfa8f2b7feda908f9fa0496294084
RichBu <RichBu@hotmail.com>
2020-08-23 15:01:01 -0500

    More camera options working
    Displays image at lower res while taking a high res picture.

--- VisualInspec/Form_Picture.Designer.cs
+++ VisualInspec/Form_Picture.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxScreen = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRaw = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.bttnStart = new System.Windows.Forms.Button();
             this.bttnSave = new System.Windows.Forms.Button();
+            this.bttnStart = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRaw)).BeginInit();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,20 +45,31 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBoxScreen);
+            this.panel1.Controls.Add(this.pictureBoxRaw);
             this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(775, 318);
+            this.panel1.Size = new System.Drawing.Size(640, 480);
             this.panel1.TabIndex = 0;
             // 
-            // pictureBox1
+            // pictureBoxScreen
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(775, 318);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxScreen.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxScreen.Name = "pictureBoxScreen";
+            this.pictureBoxScreen.Size = new System.Drawing.Size(640, 480);
+            this.pictureBoxScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxScreen.TabIndex = 0;
+            this.pictureBoxScreen.TabStop = false;
+            // 
+            // pictureBoxRaw
+            // 
+            this.pictureBoxRaw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxRaw.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxRaw.Name = "pictureBoxRaw";
+            this.pictureBoxRaw.Size = new System.Drawing.Size(640, 480);
+            this.pictureBoxRaw.TabIndex = 1;
+            this.pictureBoxRaw.TabStop = false;
             // 
             // panel2
             // 
@@ -64,20 +77,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.Controls.Add(this.bttnSave);
             this.panel2.Controls.Add(this.bttnStart);
-            this.panel2.Location = new System.Drawing.Point(13, 338);
+            this.panel2.Location = new System.Drawing.Point(13, 547);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(775, 100);
+            this.panel2.Size = new System.Drawing.Size(944, 100);
             this.panel2.TabIndex = 1;
-            // 
-            // bttnStart
-            // 
-            this.bttnStart.Location = new System.Drawing.Point(16, 37);
-            this.bttnStart.Name = "bttnStart";
-            this.bttnStart.Size = new System.Drawing.Size(90, 42);
-            this.bttnStart.TabIndex = 1;
-            this.bttnStart.Text = "&Start";
-            this.bttnStart.UseVisualStyleBackColor = true;
-            this.bttnStart.Click += new System.EventHandler(this.bttnStart_Click);
             // 
             // bttnSave
             // 
@@ -89,17 +92,30 @@
             this.bttnSave.UseVisualStyleBackColor = true;
             this.bttnSave.Click += new System.EventHandler(this.bttnSave_Click);
             // 
+            // bttnStart
+            // 
+            this.bttnStart.Location = new System.Drawing.Point(16, 37);
+            this.bttnStart.Name = "bttnStart";
+            this.bttnStart.Size = new System.Drawing.Size(90, 42);
+            this.bttnStart.TabIndex = 1;
+            this.bttnStart.Text = "Start";
+            this.bttnStart.UseVisualStyleBackColor = true;
+            this.bttnStart.Click += new System.EventHandler(this.bttnStart_Click);
+            // 
             // Form_Picture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(969, 659);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form_Picture";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Picture_FormClosing);
+            this.Load += new System.EventHandler(this.Form_Picture_Load);
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRaw)).EndInit();
             this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -108,10 +124,11 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxScreen;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button bttnStart;
         private System.Windows.Forms.Button bttnSave;
+        private System.Windows.Forms.PictureBox pictureBoxRaw;
     }
 }
 
--- VisualInspec/Form_Picture.cs
+++ VisualInspec/Form_Picture.cs
@@ -10,6 +10,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//for camera
+using System.Media; // Windows.Media.Capture;
+
 
 // Important: include the opencvsharp library in your code
 using OpenCvSharp;
@@ -18,14 +21,37 @@ using OpenCvSharp.Extensions;
 
 namespace VisualInspec
 {
+ 
     public partial class Form_Picture : Form
     {
         // Create class-level accesible variables
         VideoCapture capture;
         Mat frame;
+        Mat frameRaw;    //raw full resolution from camera
         Bitmap image;
+        Bitmap imageRaw; //raw image from the camera
         private Thread camera;
         bool isCameraRunning = false;
+        bool isCameraPaused = false;
+        int[] imgHor = { 640, 1280, 1280 };  //1=all  2=HP max  3=Laptop max
+        int[] imgVert = { 480, 960, 720 };   //1=all  2=HP max  3=Laptop max
+        const int CamNum = 2;
+        const int CV_CAP_PROP_FRAME_WIDTH = 3;
+        const int CV_CAP_PROP_FRAME_HEIGHT = 4;
+
+        ConfigDataType ConfigData;
+
+
+        //
+        private ConfigDataType ConfigData_Init()
+        {
+            //setup a new config data
+            ConfigDataType outputRec = new ConfigDataType();
+            outputRec.CameraToUse = 0;
+            outputRec.isMsgBoxPopUp = true;
+
+            return outputRec;
+        }
 
 
         // Declare required methods
@@ -37,23 +63,35 @@ namespace VisualInspec
 
         private void CaptureCameraCallback()
         {
-
-            frame = new Mat();
+            Console.WriteLine("vert=" + imgVert[CamNum]);
+            frame = new Mat(imgVert[CamNum], imgHor[CamNum], 0 );  //type 2=16bit unsigned
+            frameRaw = new Mat(imgVert[CamNum], imgHor[CamNum], 0);  //type 2=16bit unsigned
             capture = new VideoCapture(0);
             capture.Open(0);
+            capture.Set(CV_CAP_PROP_FRAME_HEIGHT, imgVert[CamNum]);
+            capture.Set(CV_CAP_PROP_FRAME_WIDTH, imgHor[CamNum]);
 
             if (capture.IsOpened())
             {
-                while (isCameraRunning)
+                while (isCameraRunning && !isCameraPaused)
                 {
-
                     capture.Read(frame);
                     image = BitmapConverter.ToBitmap(frame);
-                    if (pictureBox1.Image != null)
+                    //image = BitM
+                    if (pictureBoxScreen.Image != null)
                     {
-                        pictureBox1.Image.Dispose();
+                        pictureBoxScreen.Image.Dispose();
+                    };
+                    pictureBoxScreen.Image = image;  //don't need to rescale, picture box does it
+
+                    //raw image to save to disk
+                    //capture.Read(frame);
+                    imageRaw = BitmapConverter.ToBitmap(frame);
+                    if (pictureBoxRaw.Image != null)
+                    {
+                        pictureBoxRaw.Image.Dispose();
                     }
-                    pictureBox1.Image = image;
+                    pictureBoxRaw.Image = imageRaw;
                 }
             }
         }
@@ -63,36 +101,88 @@ namespace VisualInspec
             InitializeComponent();
         }
 
+
+        public void CameraStop()
+        {
+            isCameraRunning = false;
+            if (capture != null)
+            {
+                capture.Release();
+            };
+            bttnStart.Text = "Start";
+            isCameraRunning = false;
+        }
+
+
+        public void CameraStart()
+        {
+            CaptureCamera();
+            bttnStart.Text = "Stop";
+            isCameraRunning = true;
+            isCameraPaused = false;
+        }
+
+
+        public void CameraPause()
+        {
+            isCameraPaused = true;
+            bttnStart.Text = "Paused";
+        }
+
+
+        public void CameraUnPause()
+        {
+            isCameraPaused = false;
+            bttnStart.Text = "Stop";
+        }
+
+
         private void bttnStart_Click(object sender, EventArgs e)
         {
             if (bttnStart.Text.Equals("Start"))
             {
-                CaptureCamera();
-                bttnStart.Text = "Stop";
-                isCameraRunning = true;
+                CameraStart();
             }
             else
             {
-                if (capture!=null)
-                {
-                    capture.Release();
-                };
-                bttnStart.Text = "Start";
-                isCameraRunning = false;
+                CameraStop();
             }
         }
+
+        private void Shutdown()
+        {
+            //shutdown everything
+            CameraStop();
+            Application.DoEvents();
+            pictureBoxScreen.Image.Dispose();
+            Application.DoEvents();
+            Application.Exit();
+        }
+
 
         private void bttnSave_Click(object sender, EventArgs e)
         {
             if (isCameraRunning)
             {
                 // Take snapshot of the current image generate by OpenCV in the Picture Box
-                Bitmap snapshot = new Bitmap(pictureBox1.Image);
+                CameraPause();
+                Bitmap snapshot = new Bitmap(pictureBoxRaw.Image);
 
                 // Save in some directory
                 // in this example, we'll generate a random filename e.g 47059681-95ed-4e95-9b50-320092a3d652.png
                 // snapshot.Save(@"C:\Users\sdkca\Desktop\mysnapshot.png", ImageFormat.Png);
                 snapshot.Save(string.Format(@"C:\Data\Junk\{0}.png", Guid.NewGuid()), ImageFormat.Png);
+                Application.DoEvents();
+                if (ConfigData.isMsgBoxPopUp)
+                {
+                    MessageBox.Show("Save successful");
+                };
+                Application.DoEvents();
+                CameraUnPause();
+                Application.DoEvents();
+                CameraStop();
+                Application.DoEvents();
+                CameraStart();
             }
             else
             {
@@ -100,5 +190,22 @@ namespace VisualInspec
             }
         }
 
+        private void Form_Picture_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Shutdown();
+            Application.DoEvents();
+        }
+
+        private void Form_Picture_Load(object sender, EventArgs e)
+        {
+            ConfigData = ConfigData_Init();
+        }
     }
+
+    public class ConfigDataType
+    {
+        public bool isMsgBoxPopUp { get; set; } //should msg box would pop 
+        public int CameraToUse { get; set; }    //which camera, on table 0=back
+    }
+
 }
